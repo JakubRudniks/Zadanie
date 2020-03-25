d97fec6b3b844b8f7048ef711d41f51a2839d4b5
JakubRudniks <62304977+JakubRudniks@users.noreply.github.com>
2020-03-25 10:22:18 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+gra  pierwsza;
+	cout<<"podaj tutu³:";
+    cin>>jeden.tytul;
+    cout<<"podaj gatunek:";
+    cin>>jeden.gatunek;
+    cout<<"wpisz ocenê:";
+    cin>>jeden.ocena;
+
+    cout<<jeden.tytul<<endl;
+    cout<<jeden.gatunek<<endl;
+    cout<<jeden.ocena<<endl;
+
+gra druga;
+    cout<<"podaj tutu³:";
+    cin>>dwa.tytul;
+    cout<<"podaj gatunek:";
+    cin>>dwa.gatunek;
+    cout<<"wpisz ocenê:";
+    cin>>dwa.ocena;
+
+    cout<<dwa.tytul<<endl;
+    cout<<dwa.gatunek<<endl;
+    cout<<dwa.ocena<<endl;
+
+    return 0;
+}
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
